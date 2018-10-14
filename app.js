87458f898aa282450c41d2b3a510bf15e7171a36
zjaredkhan <jared.khan@outlook.com>
2018-10-13 18:30:14 -0700

    still working on JS, still not working though

--- app.js
+++ app.js
@@ -2,6 +2,11 @@
 
 //global Variables
 
+//place holders for images, not sure if will be used
+var left;
+var middle;
+var right;
+
 //array for all products
 var productImages = [];
 
@@ -29,7 +34,7 @@ var boots = new Products('Rain boots', 'img/boots.jpg');
 var bubblegum = new Products('Meatball gum', 'img/bubblegum.jpg');
 var chair = new Products('Red chair', 'img/chair.jpg');
 var cthulhu = new Products('Cthulhu figurine', 'img/cthulhu.jpg');
-var dragon = new Products('Dragon meat', 'img/dragon');
+var dragon = new Products('Dragon meat', 'img/dragon.jpg');
 var pen = new Products('Utensil pen', 'img/pen.jpg');
 var petSweet = new Products('Pet Sweeper', 'img/pet-sweep.jpg');
 var shark = new Products('Shark sleeping bag', 'img/shark.jpg');
@@ -38,9 +43,65 @@ var unicorn = new Products('Unicorn meat', 'img/unicorn.jpg');
 var usb = new Products('USB tentacle', 'img/usb.gif');
 var waterCan = new Products('Watering can', 'img/water-can.jpg');
 
+//Image number variable needed for Render function (hope I'm doing this right!)
+var imageRandom1;
+var imageRandom2;
+var imageRandom3;
 
+//create random function
+var randomProduct = function() {
+  return Math.floor(Math.random() * productImages.length);
+};
 
+//Render function, might need to reorder function
+var renderProducts = function() {
+  
+  imageRandom1 = randomProduct();
+  //line below is to pull the randomly selected image out of the array
+  imageLeft = productImages[imageRandom1].imgLocation;
+  //incrememnt the counter!
+  productImages[imageRandom1].timesDisplayed ++;
+  imageRandom2 = randomProduct();
+  //need a while loop so that imageRandom2 != imageRandom1 (no dupe images inline, that'd be lame)
+  while (imageRandom2 === imageRandom1){
+    imageRandom2 = randomProduct();
+  }
+  imageMiddle = productImages[imageRandom2].imgLocation;
+  productImages[imageRandom2].timesDisplayed ++;
+  imageRandom3 = randomProduct();
+  while (imageRandom3 === imageRandom2 || imageRandom3 === imageRandom1){
+    imageRandom3 = randomProduct();
+  }
+  imageRight = productImages[imageRandom3].imgLocation;
+  productImages[imageRandom3].timesDisplayed ++;
+  var imageLeft = document.getElementById('left');
+  var imageMiddle = document.getElementById('middle');
+  var imageRight = document.getElementById('right');
+};
+console.log(imageLeft);
 
+//click handler function
+function handleClick(image){
+  image.productClickCount += 1;
+  totalClicks += 1;
+  renderProducts();
+}
+
+//event handlers - use ids from html DOM
+
+imageLeft.addEventListener('click', function(){
+  handleClick(productImages[imageRandom1]);
+});
+
+imageMiddle.addEventListener('click', function(){
+  handleClick(productImages[imageRandom2]);
+});
+
+imageRight.addEventListener('click', function(){
+  handleClick(productImages[imageRandom3]);
+});
+
+renderProducts();
 //Stuff below is from original goat demo
 
 // //global vars
