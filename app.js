c1952e70ad20c2e0e3050d39f2c14198cd891199
zjaredkhan <jared.khan@outlook.com>
2018-10-20 10:23:00 -0700

    got image rendering to work

--- app.js
+++ app.js
@@ -3,50 +3,78 @@
 //global Variables
 
 //place holders for images, not sure if will be used
-var left;
-var middle;
-var right;
+// var left;
+// var middle;
+// var right;
 
 //array for all products
 var productImages = [];
 
 //total votes (for counting to 25)
 var totalClicks = 0;
-
-//image place holders for diplaying on html
-var imageLeft = document.getElementById('left');
-var imageMiddle = document.getElementById('middle');
-var imageRight = document.getElementById('right');
+console.log(totalClicks);
 
 //constructor function for products
-function Products (productName,imgLocation){
+function Products (productName,src){
   this.productName = productName;
-  this.imgLocation = imgLocation;
+  this.src = src;
   this.productClickCount = 0;
   this.timesDisplayed = 0;
   productImages.push(this);
 }
+//image place holders for diplaying on html
+var imageLeft = document.getElementById('left');
+console.log('here', imageLeft);
+//imageLeft.src = productImages[imageRandom1].src;
+//imageLeft.alt = productImages[imageRandom1].productName;
+
+var imageMiddle = document.getElementById('middle');
+//imageMiddle.src = productImages[imageRandom2].src;
+//imageMiddle.alt = productImages[imageRandom2].productName;
+
+var imageRight = document.getElementById('right');
+//imageRight.src = productImages[imageRandom3].src;
+//imageRight.alt = productImages [imageRandom3].productName;
+
 
 //constructing the product objects
-var bag = new Products('Super cool bag', 'img/bag.jpg');
-var banana = new Products('Banana slicer', 'img/banana.jpg');
-var boots = new Products('Rain boots', 'img/boots.jpg');
-var bubblegum = new Products('Meatball gum', 'img/bubblegum.jpg');
-var chair = new Products('Red chair', 'img/chair.jpg');
-var cthulhu = new Products('Cthulhu figurine', 'img/cthulhu.jpg');
-var dragon = new Products('Dragon meat', 'img/dragon.jpg');
-var pen = new Products('Utensil pen', 'img/pen.jpg');
-var petSweet = new Products('Pet Sweeper', 'img/pet-sweep.jpg');
-var shark = new Products('Shark sleeping bag', 'img/shark.jpg');
-var sweep = new Products('Crawler sweeper', 'img/sweep.png');
-var unicorn = new Products('Unicorn meat', 'img/unicorn.jpg');
-var usb = new Products('USB tentacle', 'img/usb.gif');
-var waterCan = new Products('Watering can', 'img/water-can.jpg');
+var bag = new Products('Super cool bag', './img/bag.jpg');
+var banana = new Products('Banana slicer', './img/banana.jpg');
+var boots = new Products('Rain boots', './img/boots.jpg');
+var bubblegum = new Products('Meatball gum', './img/bubblegum.jpg');
+var chair = new Products('Red chair', './img/chair.jpg');
+var cthulhu = new Products('Cthulhu figurine', './img/cthulhu.jpg');
+var dragon = new Products('Dragon meat', './img/dragon.jpg');
+var pen = new Products('Utensil pen', './img/pen.jpg');
+var petSweet = new Products('Pet Sweeper', './img/pet-sweep.jpg');
+var shark = new Products('Shark sleeping bag', './img/shark.jpg');
+var sweep = new Products('Crawler sweeper', './img/sweep.png');
+var unicorn = new Products('Unicorn meat', './img/unicorn.jpg');
+var usb = new Products('USB tentacle', './img/usb.gif');
+var waterCan = new Products('Watering can', './img/water-can.jpg');
 
-//Image number variable needed for Render function (hope I'm doing this right!)
-var imageRandom1;
-var imageRandom2;
-var imageRandom3;
+
+console.log(productImages[3].src);
+
+// console.log(bag);
+// console.log(banana);
+// console.log(boots);
+// console.log(bubblegum);
+// console.log(chair);
+// console.log(cthulhu);
+// console.log(dragon);
+// console.log(pen);
+// console.log(petSweet);
+// console.log(shark);
+// console.log(sweep);
+// console.log(unicorn);
+// console.log(usb);
+// console.log(waterCan);
+
+//Image number variable needed for Render function
+var imageRandom1 = 0;
+var imageRandom2 = 0;
+var imageRandom3 = 0;
 
 //create random function
 var randomProduct = function() {
@@ -55,10 +83,13 @@ var randomProduct = function() {
 
 //Render function, might need to reorder function
 var renderProducts = function() {
-  
+  // var imageLeft = document.getElementById('left');
+  // var imageMiddle = document.getElementById('middle');
+  // var imageRight = document.getElementById('right');
   imageRandom1 = randomProduct();
   //line below is to pull the randomly selected image out of the array
-  imageLeft = productImages[imageRandom1].imgLocation;
+  console.log(productImages[imageRandom1].src);
+  imageLeft.src = productImages[imageRandom1].src;
   //incrememnt the counter!
   productImages[imageRandom1].timesDisplayed ++;
   imageRandom2 = randomProduct();
@@ -66,42 +97,50 @@ var renderProducts = function() {
   while (imageRandom2 === imageRandom1){
     imageRandom2 = randomProduct();
   }
-  imageMiddle = productImages[imageRandom2].imgLocation;
+  imageMiddle.src = productImages[imageRandom2].src;
   productImages[imageRandom2].timesDisplayed ++;
   imageRandom3 = randomProduct();
   while (imageRandom3 === imageRandom2 || imageRandom3 === imageRandom1){
     imageRandom3 = randomProduct();
   }
-  imageRight = productImages[imageRandom3].imgLocation;
+  imageRight.src = productImages[imageRandom3].src;
   productImages[imageRandom3].timesDisplayed ++;
-  var imageLeft = document.getElementById('left');
-  var imageMiddle = document.getElementById('middle');
-  var imageRight = document.getElementById('right');
 };
-console.log(imageLeft);
 
-//click handler function
-function handleClick(image){
-  image.productClickCount += 1;
-  totalClicks += 1;
-  renderProducts();
-}
+// //click handler function
+// function handleClick(image){
+//   image.productClickCount += 1;
+//   totalClicks += 1;
+//   renderProducts();
+// }
 
-//event handlers - use ids from html DOM
+// //event handlers - use ids from html DOM
 
-imageLeft.addEventListener('click', function(){
-  handleClick(productImages[imageRandom1]);
-});
+// document.addEventListener('click', function(){
+//   handleClick(productImages[imageRandom1]);
+// });
 
-imageMiddle.addEventListener('click', function(){
-  handleClick(productImages[imageRandom2]);
-});
+// document.addEventListener('click', function(){
+//   handleClick(productImages[imageRandom2]);
+// });
 
-imageRight.addEventListener('click', function(){
-  handleClick(productImages[imageRandom3]);
-});
+// document.addEventListener('click', function(){
+//   handleClick(productImages[imageRandom3]);
+// });
 
+
+// document.addEventListener("click", function(){
+//   document.getElementById("demo").innerHTML = "Hello World!";
+// });
 renderProducts();
+console.log(imageLeft.src);
+
+
+
+console.log(productImages[3].src);
+
+
+
 //Stuff below is from original goat demo
 
 // //global vars
